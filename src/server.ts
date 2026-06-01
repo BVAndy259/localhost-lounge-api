04b3ce6d8752c29f77a7444818a44e49b5068a75
BVAndy259 <andicito183@gmail.com>
2026-06-01 16:30:11 -0500

    fix: Correction of CORS data for accepted routes

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import app from './app';
 import { env } from './config/env';
 import { logger } from './utils/logger';
 
+const serverUrl = process.env.RENDER_EXTERNAL_URL || `http://localhost:${env.PORT}`;
+
 app.listen(env.PORT, () => {
   logger.info(`[SERVER] LocalHost Lounge API está disponible`);
   logger.info(`[SERVER] Corriendo en el puerto: ${env.PORT}`);
-  logger.info(`[SERVER] Health Check: http://localhost:${env.PORT}/api/health`);
+  logger.info(`[SERVER] Health Check: ${serverUrl}/api/health`);
 });
