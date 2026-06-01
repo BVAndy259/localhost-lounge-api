fbd4c493006e0c9a4c2b8846ebbdaeb5ddc9254f
BVAndy259 <andicito183@gmail.com>
2026-06-01 16:08:55 -0500

    fix: elimination of initialization variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,9 +11,6 @@ export const env = {
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
   OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY as string,
-  ADMIN_NAME: process.env.ADMIN_NAME as string,
-  ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
-  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
 };
 
 if (!env.DATABASE_URL || !env.DIRECT_URL) {
@@ -22,10 +19,6 @@ if (!env.DATABASE_URL || !env.DIRECT_URL) {
 
 if (!env.JWT_SECRET) {
   throw new Error('[ENV ERROR] Falta JWT_SECRET en el archivo .env');
-}
-
-if (!env.ADMIN_NAME || !env.ADMIN_EMAIL || !env.ADMIN_PASSWORD) {
-  throw new Error('[ENV ERROR] Falta nombre, email o contraseña en el archivo .env');
 }
 
 if (!env.CLOUDINARY_CLOUD_NAME || !env.CLOUDINARY_API_KEY || !env.CLOUDINARY_API_SECRET) {
