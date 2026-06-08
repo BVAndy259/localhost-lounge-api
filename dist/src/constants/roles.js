70eefa44ff3a657e2aebbcca98ef654c06306858
BVAndy259 <andicito183@gmail.com>
2026-06-08 12:54:14 -0500

    fix:

--- dist/src/constants/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["ADMIN"] = "ADMIN";
-    Roles["RECEPCIONISTA"] = "RECEPCIONISTA";
-})(Roles || (exports.Roles = Roles = {}));
